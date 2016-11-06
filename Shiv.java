19fe0abaabfdc5344c231b3e792618f637ca0a15
Shivam Kumar <shivamhclme5@gmail.com>
2016-11-06 16:39:40 +0530

    Add files via upload

---
+++ Shiv.java
@@ -0,0 +1,60 @@
+
+public class Shiv {
+     public static void main(String[] args) {
+		char a1 = 'A';
+		System.out.println("65-"+a1);
+		 a1 = 'B';
+		System.out.println("66-"+a1);
+		 a1 = 'C';
+		System.out.println("67-"+a1);
+		 a1 = 'D';
+		System.out.println("68-"+a1);
+		 a1 = 'E';
+		System.out.println("69-"+a1);
+		 a1 = 'F';
+		System.out.println("70-"+a1);
+		 a1 = 'G';
+		System.out.println("71-"+a1);
+		 a1 = 'H';
+		System.out.println("72-"+a1);
+	    a1 = 'I';
+		System.out.println("73-"+a1);
+	    a1 = 'J';
+		System.out.println("74-"+a1);
+	    a1 = 'K';
+		System.out.println("75-"+a1);
+	    a1 = 'L';
+		System.out.println("76-"+a1);
+		 a1 = 'M';
+		System.out.println("77-"+a1);
+		 a1 = 'N';
+		System.out.println("78-"+a1);
+		 a1 = 'O';
+		System.out.println("79-"+a1);
+		 a1 = 'P';
+		System.out.println("80-"+a1);
+		 a1 = 'Q';
+		System.out.println("81-"+a1);
+		 a1 = 'R';
+		System.out.println("82-"+a1);
+		 a1 = 'S';
+		System.out.println("83-"+a1);
+		 a1 = 'T';
+		System.out.println("84-"+a1);
+		 a1 = 'U';
+		System.out.println("85-"+a1);
+		 a1 = 'V';
+		System.out.println("86-"+a1);
+		 a1 = 'W';
+		System.out.println("87-"+a1);
+		 a1 = 'X';
+		System.out.println("88-"+a1);
+		 a1 = 'Y';
+		System.out.println("89-"+a1);
+		 a1 = 'Z';
+		System.out.println("90-"+a1);
+		
+		
+	}
+	
+}
